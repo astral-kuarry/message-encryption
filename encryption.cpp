023f53c008aacbf03188648db4c44ee441913252
Dante <dsanaei@purdue.edu>
2019-06-26 19:10:58 -0400

    First Commit

---
+++ encryption.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : encryption.cpp
+// Author      : Dante Sanaei and Alec Leven
+// Version     :
+// Copyright   : MIT
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "This is the beginning of the end" << endl;
+	return 0;
+}

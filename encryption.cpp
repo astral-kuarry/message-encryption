f8fe41e7da3e8718af5a89b6ea90f4d8cf29b4ee
Dante <dsanaei@purdue.edu>
2019-06-29 17:15:07 -0400

    Add Input Function

--- encryption.cpp
+++ encryption.cpp
@@ -6,27 +6,60 @@
 //============================================================================
 
 #include <iostream>
+#include <stdio.h>
 #include "username.h"
+#include "stdlib.h"
+
 
 using namespace std;
 
 //functions
 void intro();
-void input();
+void inputs(string *email, string *message, string *receivedKey, int *type);
+void email(string email, string message, string receivedKey, int type);
+
 
 int main() {
-	input();
-	return 0;
-}
- 
-void input(){ //Return email, message, time, and recieved keyword if recipient
 	string email;
 	string message;
-	int time;
+	string receivedKey;
 	int type;
-	int ampm;
-	cout << "Welcome to AQ Encryption v1.0" << endl;
-	cout << "Enter email of recipient: ";
-	cin >> email;
-	cout << "Enter message: ";
+	inputs(&email, &message, &receivedKey, &type);
+	return 0;
 }
+
+// Prompts the user for inputs
+void inputs(string *email, string *message, string *receivedKey, int *type) { //Return email, message, time, and recieved keyword if recipient
+	system("clear");
+	cout << "Welcome to AQ Encryption v1.0" << endl;
+	cout << "Enter your status: \n[1] Sender \n[2] Receiver\n";
+	cin >> *type;
+	if (cin.fail()) {
+		*type = 0;
+	}
+	if (*type == 1){
+	cout << "Enter email of recipient: ";
+	cin >> *email;
+	cout << "Enter message: ";
+	cin >> *message;
+	} else if (*type == 2){
+	cout << "Enter message: ";
+	cin >> *message;
+	cout << "Enter keyword: ";
+	cin >> *receivedKey;
+	cout << "Enter email of sender: ";
+	cin >> *email;
+	} else{
+		cout << "Invalid Input\n";
+		exit(1);
+	}
+}
+
+// Emails proper data to receiver, or emails pass/fail status to sender
+void email(string email, string message, string receivedKey, int type){
+	if (type == 1) {
+
+	}
+}
+
+

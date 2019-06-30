9c09589cc8ac75522f33a614315d2146b80b251e
Dante <dsanaei@purdue.edu>
2019-06-30 18:05:12 -0400

    Removed Non Alphanumeric Characters

--- dante.cpp
+++ dante.cpp
@@ -14,6 +14,7 @@ void printArray(char arr[], int size);
 string intToString (int arry[], int size);
 string getTextKey(string message);
 int countSpaces( char str[], int n );
+int stringRemoveNonAlphaNum(char *str);
 
 int main() {
     int mark[5] = {19, 10, 8, 17, 9};
@@ -25,23 +26,17 @@ int main() {
     //cout << intToString(test, size) << "\n";
     cout << getTextKey("People who have met their online friends, what made you instantly regret it?") << "\n";
     cout << getTextKey("What are some things you realised too late in life?") << "\n";
-    cout << getTextKey("\"Bottas is the meat in a ferrari sandwitch ")<< "\n";
-    cout << getTextKey("e ")<< "\n";
-    cout << getTextKey(" sd d d")<< "\n";
+    cout << getTextKey("\"Bott!23as is the meat in a ferrari sandwitch ")<< "\n";
+    cout << getTextKey("  test§¶•hªº–≠ test ")<< "\n";
+    cout << getTextKey("Formula.1.2019x09.Austria.Race.SkyF1HD.Smcgill1969")<< "\n";
     cout << getTextKey("ewer wer wer werw erw erw er wer wer wer wer wer wer wer werwer wer wer ")<< "\n";
-    cout << getTextKey("                     werwerwere")<< "\n";
+    cout << getTextKey("  123werwerwere")<< "\n";
 
-    //printf("%s\n", intToString(test, size));
 
     //string encoded = base64_encode(reinterpret_cast<const unsigned char*>(s.c_str()), s.length());
     //cout << "\n" << encoded << "\n";
 
 
-    //  You can also assign directly to a string.
-    //str = "This is another string";
-
-    // or
-    //str = arr;
 	return 0;
 }
 void swap(int *xp, int *yp) 
@@ -67,6 +62,7 @@ void printArray(char arr[], int size)
     printf("\n"); 
 } 
 
+//Converts int array to a string
 string intToString (int arry[], int size){
     string outputString = "";
     for (int i = 0; i < size; i++){
@@ -76,7 +72,7 @@ string intToString (int arry[], int size){
     return outputString;
 }
 
- 
+//Get text from user and converts it to TextKey
 string getTextKey(string message){ 
     int i, j, k, l, m;
     char messageArry[message.size()+1];
@@ -86,7 +82,7 @@ string getTextKey(string message){
     for(unsigned int iLoop = 0; iLoop < size; iLoop++ )
         if(messageArry [iLoop] == ' ' )
             spaces++;
-    char finalArry[10];
+    char finalArry[11];
     int count = 0; 
     for (i = 0; messageArry[i]; i++) 
         if (messageArry[i] != ' ') 
@@ -113,6 +109,29 @@ string getTextKey(string message){
         }
     }
     finalArry[10] = '\0';
-    return string(finalArry);
-    
+    int loops = stringRemoveNonAlphaNum(finalArry);
+    if (loops > 0){
+        for (int t = 9-loops+1; t < 10; t++){
+            finalArry[t] = 'X';
+        }     
+    }
+    string finalString = string(finalArry);
+    return finalString;
+}
+int stringRemoveNonAlphaNum(char *str)
+{
+    unsigned long i = 0;
+    unsigned long j = 0;
+    char c;
+    int loops = 0;
+    while ((c = str[i++]) != '\0')
+    {
+        if (isalnum(c) && !isdigit(c))
+        {
+            str[j++] = c;
+        }else{
+            loops++;
+        }
+    }
+    return loops;
 }

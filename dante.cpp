f16f91385fc4c21a9ad22d08e0f3b6299a72454e
Dante <dsanaei@purdue.edu>
2019-06-30 16:36:13 -0400

    getTextKey()

--- dante.cpp
+++ dante.cpp
@@ -2,23 +2,35 @@
 
 #include <iostream>
 #include <string>
+#include <array>
 #include <stdio.h>
+#include <cstring>
 
 using namespace std;
 
 void swap(int *xp, int *yp);
 void sort(int arr[], int n);
-void printArray(int arr[], int size);
+void printArray(char arr[], int size);
 string intToString (int arry[], int size);
+string getTextKey(string message);
+int countSpaces( char str[], int n );
 
 int main() {
     int mark[5] = {19, 10, 8, 17, 9};
     sort(mark, 5);
-    printArray(mark, 5);
+    //printArray(mark, 5);
     string s = "THIS IS A TEST";
     int test[] = {1,2,3,4,5,34,234,234,13,23,23,2};
     int size = sizeof(test)/sizeof(test[0]);
-    cout << intToString(test, size) << "\n";
+    //cout << intToString(test, size) << "\n";
+    cout << getTextKey("People who have met their online friends, what made you instantly regret it?") << "\n";
+    cout << getTextKey("What are some things you realised too late in life?") << "\n";
+    cout << getTextKey("\"Bottas is the meat in a ferrari sandwitch ")<< "\n";
+    cout << getTextKey("e ")<< "\n";
+    cout << getTextKey(" sd d d")<< "\n";
+    cout << getTextKey("ewer wer wer werw erw erw er wer wer wer wer wer wer wer werwer wer wer ")<< "\n";
+    cout << getTextKey("                     werwerwere")<< "\n";
+
     //printf("%s\n", intToString(test, size));
 
     //string encoded = base64_encode(reinterpret_cast<const unsigned char*>(s.c_str()), s.length());
@@ -43,12 +55,11 @@ void sort(int arr[], int n)
 { 
    int i, j; 
    for (i = 0; i < n-1; i++)       
-       // Last i elements are already in place    
        for (j = 0; j < n-i-1; j++)  
            if (arr[j] > arr[j+1]) 
               swap(&arr[j], &arr[j+1]); 
 } 
-void printArray(int arr[], int size) 
+void printArray(char arr[], int size) 
 { 
     int i; 
     for (i=0; i < size; i++) 
@@ -63,4 +74,45 @@ string intToString (int arry[], int size){
         outputString = outputString + str;
     }
     return outputString;
+}
+
+ 
+string getTextKey(string message){ 
+    int i, j, k, l, m;
+    char messageArry[message.size()+1];
+    strcpy(messageArry, message.c_str());
+    int size = sizeof(messageArry)/sizeof(*messageArry);
+    int spaces = 0;
+    for(unsigned int iLoop = 0; iLoop < size; iLoop++ )
+        if(messageArry [iLoop] == ' ' )
+            spaces++;
+    char finalArry[10];
+    int count = 0; 
+    for (i = 0; messageArry[i]; i++) 
+        if (messageArry[i] != ' ') 
+            messageArry[count++] = messageArry[i];                    
+    messageArry[count] = '\0'; 
+    if (count > 10){
+        for (l= 0; l < 10; l++){
+            finalArry[l] = messageArry[l];
+            finalArry[l] = toupper(finalArry[l]);
+        }
+        finalArry[10] = '\0';
+    }
+    if (count < 10){
+        for ( k = 0; k < count; k++){
+            finalArry[k] = messageArry[k];
+            finalArry[k] = toupper(finalArry[k]);
+        }
+        for (l = k; l < 10; l++){
+            finalArry[l] ='X';
+        }
+    } else {
+        for (m = 0; m < 10; m++){
+            finalArry[m] = toupper(finalArry[m]);
+        }
+    }
+    finalArry[10] = '\0';
+    return string(finalArry);
+    
 }

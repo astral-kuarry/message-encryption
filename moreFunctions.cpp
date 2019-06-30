f51060054d833eaadb57997a1570af70ca4d5642
Alec Leven <AlecLeven@Alecs-MBP.attlocal.net>
2019-06-29 23:16:47 -0500

    Added getFrac, invertLetToNum, invertOrder, digitLetShift,digitNumShift

---
+++ moreFunctions.cpp
@@ -0,0 +1,107 @@
+//============================================================================
+// Name        : message-encryption
+// Author      : Dante Sanaei and Alec Leven
+// Version     : pre-release
+// Description : An algorithm to generate an encrypted username and password for a messaging service
+//============================================================================
+
+#include <iostream>
+#include <stdio.h>
+#include "username.h"
+#include "stdlib.h"
+#include <math.h>
+#include <string>
+
+using namespace std;
+
+
+//functions
+int getFrac(long double input, int numDigits);
+string invertLetToNum(string input);
+string invertOrder(string input);
+string digitLetShift(string input, int numDigits);
+string digitNumShift(string input, int numDigits);
+string intToString (int arry[], int size);
+
+
+int test();
+
+
+int main() {
+    string check = invertLetToNum("ZZZSDFKS"); //119121213526
+    printf("ans: %s\n",check.c_str());
+
+	return 0;
+}
+
+int getFrac(long double input, int numDigits) {
+    int result;
+    double intpart;
+    long double fractpart;
+
+    fractpart = modf(input , &intpart);
+    if (fractpart != 0.0) {
+        while (fractpart < 0.1) {
+            fractpart *= 10;
+        }
+    }
+        
+    for (int i = 1; i <= numDigits; i++) {
+        fractpart *= 10;
+    }
+    return round(fractpart);
+}
+string invertLetToNum(string input) {
+    char inputArry[input.size()+1];
+    strcpy(inputArry, input.c_str());
+    int numArry [input.size()];
+
+    for (int i = 0; i < sizeof(inputArry)-1; i++) {
+        numArry[i] = (int) inputArry[i] - 64;
+    }
+    int size = sizeof(numArry)/sizeof(numArry[0]);
+    string str = intToString(numArry,size);
+
+    return str;
+}
+string invertOrder(string input) {
+    char inputArry[input.size()+1];
+    strcpy(inputArry, input.c_str());
+    char dupArry [input.size()+1];
+
+    for (int i = 0; i < sizeof(inputArry); i++) {
+        dupArry[i] = inputArry[input.size()-1-i];
+    }
+    string str(dupArry);
+    return str;
+}
+string digitLetShift(string input, int numDigits){
+    char inputArry[input.size()+1];
+    strcpy(inputArry, input.c_str());
+
+    for (int i = 0; i < sizeof(inputArry)-1; i++){
+        inputArry[i] = ((((inputArry[i] - 65) + numDigits) % 26 + 26) % 26) + 65;
+    }
+    string str(inputArry);
+    return str;
+}
+string digitNumShift(string input, int numDigits){
+    char inputArry[input.size()+1];
+    strcpy(inputArry, input.c_str());
+
+    for (int i = 0; i < sizeof(inputArry)-1; i++){
+        inputArry[i] = ((((inputArry[i] - 48) + numDigits) % 10 + 10) % 10) + 48;
+    }
+    string str(inputArry);
+    return str;
+}
+string intToString (int arry[], int size){
+    string outputString = "";
+    for (int i = 0; i < size; i++){
+        std::string str = std::to_string(arry[i]);
+        outputString = outputString + str;
+        }
+    return outputString;
+}
+
+
